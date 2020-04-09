2f3f3de9922d78948f8216458052e05ab54607ba
gaggudeep <42181892+gaggudeep@users.noreply.github.com>
2020-04-09 23:29:28 +0530

    Add files via upload

--- VideoTest.py
+++ VideoTest.py
@@ -6,21 +6,22 @@ from keras.preprocessing import image
 #use in good lighting conditions and with a good camera.
 
 #load model
-#Just comment first two lines and uncomment third line to use aother model.
+#Just comment first two lines and uncomment third line to use my other model.
 model = model_from_json(open(r"archi (1).json", "r").read())
 model.load_weights(r"best_weights (1).h5")
 #model = load_model(r"my_model.h5")
 
-
+#Load the pre-trained face detector from cv2
 face_haar_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-
+#Initializing camera for real time capture
 cap=cv2.VideoCapture(0)
 
 while True:
     ret,test_img=cap.read()# captures frame and returns boolean value and captured image
     if not ret:
         continue
+    #Convert image to grayscale
     gray_img= cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
 
     faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
@@ -36,7 +37,7 @@ while True:
 
         predictions = model.predict(img_pixels)
 
-        #find max indexed array
+        #find max indexed array i.e. which emotion's probability was highest
         max_index = np.argmax(predictions[0])
 
         emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
